5fbdac7b2f340128dcad778977f22240c16e2cb1
camiska <159632556+camiska@users.noreply.github.com>
2024-03-19 13:17:49 +0100

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function sortData() {
-    // Basic test for function call
-    console.log('sortData function has been called.');
+    console.log('sortData function has been called.'); // Confirm function is called
 
-    // Test if we can access the HTML elements
-    const inputBox = document.getElementById('dataInput');
-    console.log('Data Input Box:', inputBox); // This should not be null
-
-    const testBoxes = ['chaoskeyBox', 'goldkeyBox', 'silverkeyBox', 'bronzekeyBox', 'noKeyBox'];
-    testBoxes.forEach(boxId => {
+    // Test the connection to each box individually
+    const boxIds = ['chaoskeyBox', 'goldkeyBox', 'silverkeyBox', 'bronzekeyBox', 'noKeyBox'];
+    boxIds.forEach(boxId => {
         const box = document.getElementById(boxId);
-        console.log(`${boxId}:`, box); // None of these should be null
+        console.log(`${boxId}:`, box); // This should log the HTML element, not null
         if (box) {
-            box.innerHTML = `<p>Test content for ${boxId}</p>`; // Simple content to test each box
+            box.innerHTML = `<p>Test content for ${boxId}</p>`; // Should apply if the element is correctly found
+        } else {
+            console.error('Element not found:', boxId); // Helps identify which box is missing or misnamed
         }
     });
 }
